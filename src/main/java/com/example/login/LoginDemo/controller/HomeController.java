e43af9b2bfb7e5a0a9f0e91639030c9e28f5dfb9
harishk19 <60215835+harishk19@users.noreply.github.com>
2024-03-20 17:06:39 -0500

    Update

---
+++ src/main/java/com/example/login/LoginDemo/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.login.LoginDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String Home(){
+        return "Hello Home";
+    }
+
+    @GetMapping("/secured")
+    public String secured(){
+        return "Hello Secured Home";
+    }
+
+
+}
---
+++ src/test/java/com/example/login/LoginDemo/LoginDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login.LoginDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
